b013ab364d69b70389ae6fd6e6e7b37f230aa0cc
Dave Henderson <dhenderson@gmail.com>
2020-08-16 22:04:48 -0400

    Handle nil response, and other bugfixes
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- tracing.go
+++ tracing.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"net/http"
+	"os"
 	"strings"
 
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/standard"
 	"go.opentelemetry.io/otel/api/trace"
 	exportTrace "go.opentelemetry.io/otel/sdk/export/trace"
+	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
@@ -16,6 +18,10 @@ func tagHTTPRequestHeader(h string) string  { return "http.request.header." + h 
 func tagHTTPResponseHeader(h string) string { return "http.response.header." + h }
 
 func tagsFromRequest(span trace.Span, r *http.Request) {
+	if r == nil {
+		return
+	}
+
 	for k, h := range r.Header {
 		span.SetAttribute(tagHTTPRequestHeader(k), strings.Join(h, "\n"))
 	}
@@ -23,6 +29,10 @@ func tagsFromRequest(span trace.Span, r *http.Request) {
 }
 
 func tagsFromResponse(span trace.Span, r *http.Response) {
+	if r == nil {
+		return
+	}
+
 	for k, h := range r.Header {
 		span.SetAttribute(tagHTTPResponseHeader(k), strings.Join(h, "\n"))
 	}
@@ -30,17 +40,11 @@ func tagsFromResponse(span trace.Span, r *http.Response) {
 	span.SetAttributes(standard.HTTPAttributesFromHTTPStatusCode(r.StatusCode)...)
 }
 
-func createSpan(ctx context.Context, operationName string) (context.Context, trace.Span) {
-	tracer := global.TraceProvider().Tracer("wnp-bridge")
-	spanOpts := []trace.StartOption{}
-	parent := trace.SpanFromContext(ctx)
-	if parent != nil {
-		spanOpts = append(spanOpts, trace.LinkedTo(parent.SpanContext()))
-	}
-	return tracer.Start(ctx, operationName, spanOpts...)
-}
-
 func initTracer(exporter exportTrace.SpanSyncer) error {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return fmt.Errorf("failed to lookup hostname: %w", err)
+	}
 	tp, err := sdktrace.NewProvider(
 		sdktrace.WithConfig(
 			sdktrace.Config{
@@ -48,6 +52,10 @@ func initTracer(exporter exportTrace.SpanSyncer) error {
 			},
 		),
 		sdktrace.WithSyncer(exporter),
+		sdktrace.WithResource(resource.New(
+			standard.ServiceNameKey.String("wnp-bridge"),
+			standard.ServiceInstanceIDKey.String(hostname),
+		)),
 	)
 	if err != nil {
 		return err
